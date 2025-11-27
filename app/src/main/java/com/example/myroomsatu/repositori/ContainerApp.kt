303533a6f13a47f1136fc9c4aa663d8bfe28af30
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-27 17:30:09 +0700

    menambahkan import yg ketinggalan

--- app/src/main/java/com/example/myroomsatu/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myroomsatu/repositori/ContainerApp.kt
@@ -1,5 +1,8 @@
 package com.example.myroomsatu.repositori
 
+import android.app.Application
+import android.content.Context
+import com.example.myroomsatu.DatabaseSiswa
 interface ContainerApp {
     val repositoriSiswa: RepositoriSiswa
 }
