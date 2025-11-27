180daa1e5b8c2d5597aa8304e194d6855add7d24
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-27 17:33:17 +0700

    menambahkan companion object dengan singleton instance untuk DatabaseSiswa

--- app/src/main/java/com/example/myroomsatu/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/myroomsatu/room/DatabaseSiswa.kt
@@ -8,4 +8,19 @@ import androidx.room.RoomDatabase
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
 abstract class DatabaseSiswa : RoomDatabase() {
     abstract fun siswaDao(): SiswaDao
+
+    companion object {
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
+
+        fun getDatabase(context: Context): DatabaseSiswa {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context,
+                    DatabaseSiswa::class.java,
+                    "siswaDatabase"
+                ).build().also { Instance = it }
+            }
+        }
+    }
 }
