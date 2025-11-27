34aaf2b30ebbceaca448b07d910d245c1412998c
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-27 17:28:13 +0700

    menambahkan implementasi ContainerDataApp yang menyediakan instance RepositoriSiswa

--- app/src/main/java/com/example/myroomsatu/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myroomsatu/repositori/ContainerApp.kt
@@ -3,3 +3,10 @@ package com.example.myroomsatu.repositori
 interface ContainerApp {
     val repositoriSiswa: RepositoriSiswa
 }
+class ContainerDataApp(private val context: Context) : ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            siswaDao = DatabaseSiswa.getDatabase(context).siswaDao()
+        )
+    }
+}
