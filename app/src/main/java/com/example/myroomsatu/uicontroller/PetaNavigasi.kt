6982ef353f43099ddb468e14287d2badebacbdcb
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-27 17:58:57 +0700

    menambahkan composable SiswaApp sebagai root dan penyedia NavController

---
+++ app/src/main/java/com/example/myroomsatu/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,17 @@
+package com.example.myroomsatu.uicontroller
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun SiswaApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    HostNavigasi(
+        navController = navController,
+        modifier = modifier
+    )
+}
