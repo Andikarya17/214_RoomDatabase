aa0dd12b38c0847cb05820eddcbccb4e54adaaac
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-27 18:47:06 +0700

    menambahkan PenyediaViewModel dengan factory untuk HomeViewModel dan EntryViewModel

--- app/src/main/java/com/example/myroomsatu/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myroomsatu/viewmodel/provider/PenyediaViewModel.kt
@@ -1,2 +1,29 @@
 package com.example.myroomsatu.viewmodel.provider
 
+package com.example.myroomsatu.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.myroomsatu.viewmodel.EntryViewModel
+import com.example.myroomsatu.viewmodel.HomeViewModel
+import com.example.myroomsatu.AplikasiSiswa
+
+object PenyediaViewModel {
+
+    val Factory = viewModelFactory {
+
+        initializer {
+            HomeViewModel(
+                repositoriSiswa = aplikasiSiswa().container.repositoriSiswa
+            )
+        }
+
+        initializer {
+            EntryViewModel(
+                repositoriSiswa = aplikasiSiswa().container.repositoriSiswa
+            )
+        }
+    }
+}
