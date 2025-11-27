4a629a30e1043ca5716ee912f3feb1d40d47e00d
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-27 17:20:25 +0700

    menambahkan interface RepositoriSiswa dengan fungsi dasar

--- app/src/main/java/com/example/myroomsatu/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myroomsatu/repositori/RepositoriSiswa.kt
@@ -1,2 +1,10 @@
 package com.example.myroomsatu.repositori
 
+import com.example.myroomsatu.room.Siswa
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+
+    suspend fun insertSiswa(siswa: Siswa)
+}
