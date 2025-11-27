c0ae92319827406885f1ce914c226c3f2d8a133e
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-27 17:37:06 +0700

    menambahkan fungsi getAllSiswa dan insert pada DAO untuk operasi database

--- app/src/main/java/com/example/myroomsatu/room/SiswaDao.kt
+++ app/src/main/java/com/example/myroomsatu/room/SiswaDao.kt
@@ -2,6 +2,16 @@ package com.example.myroomsatu.room
 
 import androidx.room.Dao
 import kotlinx.coroutines.flow.Flow
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
-interface SiswaDao
+interface SiswaDao {
+
+    @Query("SELECT * FROM siswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
+}
