c841abf9ad67481c58ec0b8888206e463255ce89
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-27 17:43:17 +0700

    menambahkan DestinasiEntry

---
+++ app/src/main/java/com/example/myroomsatu/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.myroomsatu.route
+
+import com.example.myroomsatu.R
+
+object DestinasiEntry {
+    override val route: String = "item_entry"
+    override val titleRes: Int = R.string.tambah_siswa
+}
