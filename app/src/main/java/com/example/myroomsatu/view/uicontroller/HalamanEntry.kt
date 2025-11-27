ea662e6b3e1e683f4b4377b34c45f06a22c1625e
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-27 18:20:49 +0700

    menambahkan import yang kurang

--- app/src/main/java/com/example/myroomsatu/view/uicontroller/HalamanEntry.kt
+++ app/src/main/java/com/example/myroomsatu/view/uicontroller/HalamanEntry.kt
@@ -1,5 +1,20 @@
 package com.example.myroomsatu.view.uicontroller
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myroomsatu.R
+import kotlinx.coroutines.launch
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EntrySiswaScreen(
