6c0a50c455ab4132415fdfba2c58f5941d07e04b
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-27 19:15:11 +0700

    perbaiki kode yg salah importnya

--- app/src/main/java/com/example/myroomsatu/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myroomsatu/repositori/ContainerApp.kt
@@ -2,7 +2,7 @@ package com.example.myroomsatu.repositori
 
 import android.app.Application
 import android.content.Context
-import com.example.myroomsatu.DatabaseSiswa
+import com.example.myroomsatu.room.DatabaseSiswa
 interface ContainerApp {
     val repositoriSiswa: RepositoriSiswa
 }
--- app/src/main/java/com/example/myroomsatu/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myroomsatu/viewmodel/provider/PenyediaViewModel.kt
@@ -6,25 +6,23 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.myroomsatu.viewmodel.EntryViewModel
 import com.example.myroomsatu.viewmodel.HomeViewModel
-import com.example.myroomsatu.AplikasiSiswa
+import com.example.myroomsatu.repositori.AplikasiSiswa
 
 object PenyediaViewModel {
 
     val Factory = viewModelFactory {
 
         initializer {
-            HomeViewModel(
-                repositoriSiswa = aplikasiSiswa().container.repositoriSiswa
-            )
+            val app = aplikasiSiswa()
+            HomeViewModel(app.container.repositoriSiswa)
         }
 
         initializer {
-            EntryViewModel(
-                repositoriSiswa = aplikasiSiswa().container.repositoriSiswa
-            )
+            val app = aplikasiSiswa()
+            EntryViewModel(app.container.repositoriSiswa)
         }
     }
 }
+
 fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
-
