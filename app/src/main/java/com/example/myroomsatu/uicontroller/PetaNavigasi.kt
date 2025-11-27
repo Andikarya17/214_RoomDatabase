3b301493e4d433b7c058c262164fb7de4173224a
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-27 18:03:23 +0700

    menambahkan HostNavigasi yang mengatur NavHost dan rute Home serta EntrySiswa

--- app/src/main/java/com/example/myroomsatu/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/myroomsatu/uicontroller/PetaNavigasi.kt
@@ -4,6 +4,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.myroomsatu.route.DestinasiEntry
+import com.example.myroomsatu.route.DestinasiHome
+
+
 
 @Composable
 fun SiswaApp(
@@ -15,3 +22,31 @@ fun SiswaApp(
         modifier = modifier
     )
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ) {
+        composable(route = DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiEntry.route)
+                }
+            )
+        }
+
+        composable(route = DestinasiEntry.route) {
+            EntrySiswaScreen(
+                navigateBack = { navController.popBackStack() }
+            )
+        }
+    }
+}
+
