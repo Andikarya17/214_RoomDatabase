08eec6a7625c5ad9a7f4dab36a10eb78ddf8b38b
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-27 18:19:02 +0700

    menambahkan layar EntrySiswaScreen dengan top bar dan logika penyimpanan data

---
+++ app/src/main/java/com/example/myroomsatu/view/uicontroller/HalamanEntry.kt
@@ -0,0 +1,39 @@
+package com.example.myroomsatu.view.uicontroller
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(id = com.example.daoapp.view.route.DestinasiEntry.titleRes),
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.saveSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
